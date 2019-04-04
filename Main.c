1367e17f58a17a0ee3001f7775a349974d0108a4
Cooper Ashton <coopera1@live.com>
2019-04-04 07:12:13 +0000

    End of lab 6 commit

--- Main.c
+++ Main.c
@@ -2,7 +2,10 @@
 
 main()
 {
-printf("Cipher")
+    int opt;
+    
+    printf(" 1 : Encrypt with rotation cipher \n 2 : Decrypt from rotation cypher \n 3 : Encrypt with substitution cipher \n 4 : Decrypt from separation cipher \n");
+    scanf("%d", &opt);
 
 }
 
